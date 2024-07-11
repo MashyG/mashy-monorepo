0044c9becff3b020e26b228179801c32b2c1ac9d
MashyG <969694772@qq.com>
2024-07-11 11:31:32 +0800

    feat: apps/next-blog add seo knowledges

---
+++ apps/next-blog/data/siteMetadata.ts
@@ -0,0 +1,8 @@
+export default {
+  title: "Mashy's Blog",
+  description: "A simple blog built with Next.js",
+  siteUrl: "http://localhost:3000",
+  author: "Mashy",
+  locale: "zh-CN",
+  socialBanner: "/images/banner.png",
+};
